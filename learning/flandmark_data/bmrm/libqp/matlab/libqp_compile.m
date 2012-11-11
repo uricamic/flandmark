b2103917662d27abf06ee433ab699202a606b4f9
Michal Uricar <uricamic@cmp.felk.cvut.cz>
2012-11-11 17:33:08 +0100

    Refactorization I: deleted old internal dir structure

--- learning/flandmark_data/bmrm/libqp/matlab/libqp_compile.m
+++
@@ -1,7 +0,0 @@
-% This script compiles Matlab's MEX interfaces for LIBQP solvers.
-%
-
-
-mex libqp_splx_mex.c -largeArrayDims ../lib/libqp_splx.c
-mex libqp_gsmo_mex.c -largeArrayDims ../lib/libqp_gsmo.c
-
--- learning/flandmark_data/libocas/version.h
+++
@@ -1,3 +0,0 @@
-/* this text will be printed in all programs */
-
-#define OCAS_VERSION "v0.93, (C) 2008, 2009, 2010 Vojtech Franc, Soeren Sonnenburg"
