3be5e8f16c424bd46ceaef3c992b87d38658f579
daniyuu <daniyuu@163.com>
2015-08-05 19:34:20 +0800

    尝试自己建立工程

--- matlab_toolbox/flandmark_simple_example.m
+++ matlab_toolbox/flandmark_simple_example.m
@@ -14,8 +14,8 @@ DIR = '../data/Images/';
 IMGS = dir([DIR '*.jpg']);
 
 % Load flandmark_model into MATLAB memory
-load('../data/model.mat')
-% model = flandmark_load_model('../data/flandmark_model.dat');
+% load('../data/model.mat')
+model = flandmark_load_model('../data/flandmark_model.dat');
 
 %% Run detector
 
